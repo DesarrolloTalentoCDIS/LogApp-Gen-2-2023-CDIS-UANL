eae532c54893989025a7b3736c297e616f9acce8
Felipe Mata <felipe_motoss@hotmail.com>
2023-06-23 09:52:25 -0600

    14 Configurar el routing y 17 Congifura las rutas
    Configurar el routing principal para las rutas. V3. 0:00-2:50
    Haz los cambios necesarios en las rutas para que se pueda visualizar el login de la anterior tarea al desplegar. V3 6:17-END

--- LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/auth.module.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './components/login/login/login.component';
+import { LoginComponent } from './components/login/login/login/login/login.component';
 import { MainComponent } from './components/main/main.component';
 import { RegisterComponent } from './components/register/register.component';
 
