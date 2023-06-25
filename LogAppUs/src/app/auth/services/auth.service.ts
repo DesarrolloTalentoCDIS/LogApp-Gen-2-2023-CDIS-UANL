9f6122f7c3c253bc989aa05a6c36bdefaf551b5a
Kevin Villalobos <kevin.villalobosvl@uanl.edu.mx>
2023-06-25 00:02:16 -0600

    19 Generar Service para el BackEnd

---
+++ LogAppUs/src/app/auth/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseURL : string = environment.baseUrl;
+  
+  constructor() { }
+}
