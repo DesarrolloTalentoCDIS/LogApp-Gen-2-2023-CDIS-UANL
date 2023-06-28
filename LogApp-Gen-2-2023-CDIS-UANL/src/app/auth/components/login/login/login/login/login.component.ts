e89117f7dc34535840c07aec0ded780549cca44f
David Arias <david.jam@hotmail.com>
2023-06-28 14:17:28 -0600

    24 Configurar service y component para notifiaciones
    Configura y añade las validaciones necesarias para mostrar las notifiaciones de acuerdo con lo comentado por el testeo de logeo. V7

--- LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/components/login/login/login/login/login.component.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/components/login/login/login/login/login.component.ts
@@ -31,7 +31,18 @@ export class LoginComponent {
     if(this.formularioLog.valid){
 
       const {id, pass} = this.formularioLog.value;
-      this.router.navigateByUrl('/dashboard');
+      this.authService.login(id,pass)
+      .subscribe(res =>{
+        if (res === true){
+          this.router.navigateByUrl('/dashboard');
+          this.toastr.success(id,'Ingreso correcto')
+        } else {
+          this.toastr.error(res, 'Error', {
+            timeOut:  4000,
+            progressAnimation: 'increasing'
+          })
+        }
+      })
 
     } else {
       this.toastr.error("Verifique sus datos", "Error");
--- LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/services/auth.service.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/services/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { enviroment } from 'src/enviroments/enviroment';
 import { user } from '../interfaces/us-interface';
+import { AuthResponse } from '../interfaces/res-interface';
+import { catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,11 +22,45 @@ export class AuthService {
   register(usname : string, id : string, pass : string ) {
     const URL = `${this.baseUrl}/auth/new` ;
     const body = {usname, id,pass};
+
+    return this.http.post<AuthResponse>(URL,body)
+    .pipe(
+      tap(res =>{
+        if(res.ok){
+          localStorage.setItem('token',res.token!)
+          this._user = {
+            id: res.id!,
+            usname: res.usname!
+          }
+        }
+      }),
+
+      map(res => res.ok),
+
+      catchError(err => of(err.error.message))
+    )
   }
   
   login(id: string, pass: string){
     const URL = `${this.baseUrl}/auth` ;
     const body = {id,pass};
+
+    return this.http.post<AuthResponse>(URL,body)
+    .pipe(
+      tap(res =>{
+        if(res.ok){
+          localStorage.setItem('token',res.token!)
+          this._user = {
+            id: res.id!,
+            usname: res.usname!
+          }
+        }
+      }),
+
+      map(res => res.ok),
+
+      catchError(err => of(err.error.message))
+    )
   }
   
   validateToken(){
