914fd6d5dc10d1f176b77ea2ab7e801174d7541b
SaulPerez165 <perezsaul165@gmail.com>
2023-06-29 14:06:27 -0600

    Corrección en la dirección del API
    Se cambió el valor de la variable baseURL.

--- LogApp-Gen-2-2023-CDIS-UANL/src/environments/environment.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    baseUrl: 'user-authentication-server-production.up.railway.app'
+    baseUrl: 'https://user-authentication-server-production.up.railway.app/api'
 };
