c8516f564344413d99553755befa53ca80f3a69c
Kevin Villalobos <kevin.villalobosvl@uanl.edu.mx>
2023-06-28 22:38:56 -0600

    25 Configura el componente de registro y validaciones

--- LogAppUs/src/app/protected-app/dashboard/dashboard.component.ts
+++ LogAppUs/src/app/protected-app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  constructor(private router: Router,
+    private authService : AuthService){
+
+  }
+
+  get usuario(){
+    return this.authService.user;
+  }
 }
