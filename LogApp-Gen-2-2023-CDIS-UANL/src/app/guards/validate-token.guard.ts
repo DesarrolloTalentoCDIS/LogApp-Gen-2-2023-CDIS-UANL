8249c0ca2be0002466cf942292fc79dc86f06368
David2003006 <davidalejandro2003@yahoo.com>
2023-06-30 20:02:25 -0700

    Tarea28

---
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/guards/validate-token.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate,CanLoad, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidateTokenGuard implements CanActivate , CanLoad{
+constructor(private authservice: AuthService,
+  private router: Router){
+
+}
+
+  canActivate( ): Observable<boolean> | boolean {
+    return this.authservice.ValidateToken().pipe(
+      tap(valid =>{
+        if(!valid){
+          this.router.navigateByUrl('/auth')
+        }
+      })
+    );
+  }
+
+  canLoad(): Observable<boolean> | boolean {
+    return this.authservice.ValidateToken.pipe(
+      tap(valid =>{
+        if(!valid){
+          this.router.navigateByUrl('/auth')
+        }
+      })
+    )
+  }
+  
+}
