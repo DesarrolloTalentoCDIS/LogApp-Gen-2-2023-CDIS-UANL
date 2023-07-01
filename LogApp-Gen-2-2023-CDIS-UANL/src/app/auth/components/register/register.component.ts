a6f9f6792eb1f5fdb2198685d31c4f14d6ee1336
David2003006 <davidalejandro2003@yahoo.com>
2023-06-30 19:34:20 -0700

    Tarea 25

--- LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/components/register/register.component.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/auth/components/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,34 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  formularioRegister: FormGroup= this.fb.group({
+      usname: ['', [Validators.required, Validators.minLength(6)]],
+      id: ['', [Validators.required, Validators.minLength(8)]],
+      pass: ['', [Validators.required, Validators.minLength(7)]],
+  });
+
+    constructor(private fb: FormBuilder,
+      private router: Router,
+      private authservices: AuthService,
+      private toastr: ToastrService
+      
+      ){}
+
+      register(){
+        const{usname, id, pass}= this.formularioRegister.value
+        this.authservices.register(usname, id, pass).subscribe(res =>{
+          if(res === true){
+            this.router.navigateByUrl("/dashboard")
+            this.toastr.success(id+""+usname, "Registro correcto")
+          }else{
+            console.log(res);
+            this.toastr.error(res, 'Error', {
+              timeOut: 4000,
+              progressAnimation: 'increasing'
+            })
+          }
+        });
+      }
+
+ 
 }
