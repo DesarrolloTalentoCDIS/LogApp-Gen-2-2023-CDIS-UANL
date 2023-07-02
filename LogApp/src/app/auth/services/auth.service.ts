f389319c42f8bfa4a7554d27b7bb0ba65f493d78
Ernesto3938 <endo3938@gmail.com>
2023-07-02 09:04:10 -0600

    V6 Login & Register Auth Service

--- LogApp/src/app/auth/services/auth.service.ts
+++ LogApp/src/app/auth/services/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { user } from '../interfaces/us-interface' ;
 import { HttpClient } from '@angular/common/http';
+import { AuthResponse } from '../interfaces/res-interface';
+import { catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +25,28 @@ export class AuthService {
     const URL  =  `${this.baseUrl}/auth/new` ;
     const body = {usname, id, pass} ;
 
+    return this.http.post<AuthResponse>(URL, body).pipe(
+
+      tap(res => {
+
+        console.log(res);
+
+        if(res.ok) {
+
+          localStorage.setItem('token', res.token!) ;
+          this._user = {
+
+            id: res.id!,
+            usname : res.usname!
+          }
+        }
+      
+      }),
+      
+      map(res => res.ok),
+      catchError(err => of(err.error.message))
+      
+      ) ;
   }
 
   login(id: string, pass: string) {
@@ -30,6 +54,28 @@ export class AuthService {
     const URL  =  `${this.baseUrl}/auth` ;
     const body = {id, pass} ;
 
+    return this.http.post<AuthResponse>(URL, body).pipe(
+
+      tap(res => {
+
+        console.log(res);
+
+        if(res.ok) {
+
+          localStorage.setItem('token', res.token!) ;
+          this._user = {
+
+            id: res.id!,
+            usname : res.usname!
+          }
+        }
+      
+      }),
+      
+      map(res => res.ok),
+      catchError(err => of(err.error.message))
+      
+      ) ;
   }
 
   validateToken () {
