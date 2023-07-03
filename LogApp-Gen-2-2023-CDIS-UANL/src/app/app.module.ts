5b70be984754d9401dd86b25f25b2264db7be6f5
ArmandoEspino <armandoerj7@gmail.com>
2023-07-03 01:51:15 -0600

    18 Generar interfaces   19 Generar Service para el BackEnd   20 Configura el Service
    Genera las interfaces que seran utiles mas adelante para establecer la forma de algunos objetos.
    
    Crea el service para la consulta a la API, ademas de  agregar la variable del API Back-End.
    
    Configura el service tomando la varibale del API del Back-End.

--- LogApp-Gen-2-2023-CDIS-UANL/src/app/app.module.ts
+++ LogApp-Gen-2-2023-CDIS-UANL/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http"
+
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     RouterModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
